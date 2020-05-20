947bc575c712519b7f7077754a911a5433651cac
陈财建 <chencaijian.chauncey@bytedance.com>
2020-05-20 15:53:08 +0800

    异常处理；日志文件；名字修改
    Change-Id: Ibfee658ee6ce32e83afbae20df4218546c5992dc

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mymicroblog
+参考https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import RotatingFileHandler
+import os
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)  # 加载配置
@@ -13,5 +16,19 @@ login = LoginManager(app)
 # 而login_view配置的就是用来login的view function。即它会用url_for去获取对应的url
 login.login_view = 'login'
 
+# 将日志打到文件中
+if not app.debug:
 
-from app import routes  # 避免循环引用，所以这一行写在后面
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)  # 每个log文件10k，保留最近10个log文件
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors  # 避免循环引用，所以这一行写在后面
